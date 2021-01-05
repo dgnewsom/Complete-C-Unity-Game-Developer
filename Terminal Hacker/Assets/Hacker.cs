0df98ca50bfe312ab41ebc83035ca24902a150c2
Daniel <Newsom>
2021-01-05 17:23:38 +0000

    Danny - Terminal Hacker complete

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    string[][] passwords = { new string[] { "books", "aisle", "shelf", "password", "borrow"} ,
+                             new string[] { "prisoner", "hancuffs", "holster", "uniform", "arrest"  } ,
+                             new string[] { "starfield", "telescope", "environment", "exploration", "astronauts" }};
+    string password;
     string name;
     int level;
     enum Screen  {Name, MainMenu, Password, Win };
@@ -18,13 +20,14 @@ public class Hacker : MonoBehaviour
     private void GetName()
     {
         currentScreen = Screen.Name;
+        Terminal.ClearScreen();
         Terminal.WriteLine("Please enter your name:");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     //Method to show main menu
@@ -46,6 +49,10 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu();
         }
+        else if(input == "name")
+        {
+            GetName();
+        }
         else
         {
             switch (currentScreen)
@@ -58,29 +65,62 @@ public class Hacker : MonoBehaviour
                     RunMainMenu(input);
                     break;
                 case Screen.Password:
+                    CheckPassword(input);
+                    break;
+                case Screen.Win:
+                    GetName();
                     break;
                 default:
                     break;
             }
+        } 
+    }
+
+    private void CheckPassword(string input)
+    {
+        if(input == password)
+        {
+            ShowWinScreen();
         }
-       
-            
+        else
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("Access Denied! - Please try again:");
+            StartGame();
+        }
+    }
+
+    private void ShowWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("**** " + GetLevelName() + " ****" + 
+                           "\nAccess Granted!\n" +
+                           "Well done " + name);
+        Terminal.WriteLine("\n\nPress enter to restart");
     }
 
     private void RunMainMenu(string input)
     {
+        int index;
         switch (input)
         {
             case "1":
-                level = 1;
+                level = 0;
+                index = Random.Range(0, passwords[level].Length);
+                password = passwords[level][index];
                 StartGame();
                 break;
             case "2":
-                level = 2;
+                level = 1;
+                index = Random.Range(0, passwords[level].Length);
+                password = passwords[level][index];
                 StartGame();
                 break;
             case "3":
-                level = 3;
+                level = 2;
+                index = Random.Range(0, passwords[level].Length);
+                password = passwords[level][index];
                 StartGame();
                 break;
             case "007":
@@ -95,6 +135,24 @@ public class Hacker : MonoBehaviour
     private void StartGame()
     {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You chose level " + level);
+        Terminal.WriteLine("\n**** " + GetLevelName() + " ****");
+        Terminal.WriteLine("Please enter the password");
+        Terminal.WriteLine("hint: " + password.Anagram());
+    }
+
+
+    private string GetLevelName()
+    {
+        switch (level)
+        {
+            case 0:
+                return "Library";
+            case 1:
+                return "Police Station";
+            case 2:
+                return "NASA";
+            default:
+                return "";
+        }
     }
 }
