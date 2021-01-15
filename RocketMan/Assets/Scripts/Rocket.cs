793995531e84b076c8af8f2dfc6ef0fd29b11ee9
Daniel <Newsom>
2021-01-15 11:20:56 +0000

    Danny - Add particle system to rocket

--- RocketMan/Assets/Scripts/Rocket.cs
+++ RocketMan/Assets/Scripts/Rocket.cs
@@ -5,71 +5,95 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidbody;
     [SerializeField]
-    float force = 25f;
-    AudioSource rocketEngine;
+    float thrustForce = 25f;
+    [SerializeField]
+    float rotationSpeed = 100f;
+    Rigidbody rigidBody;
+    AudioSource engineSound;
+    ParticleSystem engineParticleSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = this.GetComponent<Rigidbody>();
-        rocketEngine = GetComponent<AudioSource>();
+        rigidBody = this.GetComponent<Rigidbody>();
+        engineSound = GetComponent<AudioSource>();
+        engineParticleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    /*
+     * Control thrust when space key pressed
+     */
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            SetEngineBoostSound(true);
-            Boost();
+            SetEngineSound(true);
+            SetEngineParticleSystem(true);
+            rigidBody.AddRelativeForce(Vector3.up * thrustForce);
         }
         else
         {
-            SetEngineBoostSound(false);
+            SetEngineSound(false);
+            SetEngineParticleSystem(false);
         }
+    }
+
+    /*
+     * Control rotation when left and right buttons pressed
+     */
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //Take manual control of rotation
+        float rotationThisFrame = rotationSpeed * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
         {
-            RotateLeft();
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
-        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
         {
-            RotateRight();
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+        rigidBody.freezeRotation = false; // Return rotation control to physics
     }
 
-    private void RotateRight()
-    {
-        transform.Rotate(-Vector3.forward);
-    }
-
-    private void RotateLeft()
-    {
-        transform.Rotate(Vector3.forward);
-    }
-
-    private void Boost()
-    {
-        rigidbody.AddRelativeForce(0, force, 0);
-    }
-
-    private void SetEngineBoostSound(bool isBoosting)
+    /*
+     * Adjust engine sound if boosting.
+     */
+    private void SetEngineSound(bool isBoosting)
     {
         if (isBoosting)
         {
-            rocketEngine.volume = 1f;
-            rocketEngine.pitch = 1f;
+            engineSound.volume = 1f;
+            engineSound.pitch = 1f;
         }
         else
         {
-            rocketEngine.volume = 0.2f;
-            rocketEngine.pitch = 0.6f;
+            engineSound.volume = 0.2f;
+            engineSound.pitch = 0.6f;
+        }
+    }
+
+    /*
+     * Play engine particles if boosting.
+     */
+    private void SetEngineParticleSystem(bool isBoosting)
+    {
+        if (isBoosting)
+        {
+            engineParticleSystem.Play();
+        }
+        else
+        {
+            engineParticleSystem.Stop();
         }
     }
 }
