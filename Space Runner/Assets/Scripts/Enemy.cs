c9fd7e8a080ef8f958fc1163b6b7bcae42176f60
Daniel Newsom <danielgnewsom@gmail.com>
2021-05-20 18:50:45 +0100

    Danny - Add enemy HP

--- Space Runner/Assets/Scripts/Enemy.cs
+++ Space Runner/Assets/Scripts/Enemy.cs
@@ -5,33 +5,44 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemyDeathParticles;
-    [SerializeField] int scoreValue = 5;
-
+    [SerializeField] GameObject hitParticles;
+    [SerializeField] int hitScoreValue = 5;
+    [SerializeField] int startHealth = 30;
+    int currentHealth;
     ScoreBoard scoreboard;
     bool isDead = false;
+    GameObject player;
 
     private void Start()
     {
         scoreboard = FindObjectOfType<ScoreBoard>();
+        currentHealth = startHealth;
+        player = FindObjectOfType<CollisionHandler>().gameObject;
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject vfx = Instantiate(hitParticles, this.transform.position, Quaternion.identity, GameObject.Find("SpawnedItems").transform);
+        vfx.transform.LookAt(player.transform);
         if (!isDead)
         {
-            isDead = true;
             IncreaseScore();
-            KillEnemy();
+            currentHealth --;
+            if(currentHealth <= 0)
+            {
+                KillEnemy();
+            }
         }
     }
 
     private void IncreaseScore()
     {
-        scoreboard.IncreaseScore(scoreValue);
+        scoreboard.IncreaseScore(hitScoreValue);
     }
 
     private void KillEnemy()
     {
+        isDead = true;
         Instantiate(enemyDeathParticles, this.transform.position, Quaternion.identity, GameObject.Find("SpawnedItems").transform);
         Destroy(gameObject);
     }
