28ed4eadc3c7dc47d85426956191d3f021b91091
Daniel Newsom <danielgnewsom@gmail.com>
2021-05-23 11:44:19 +0100

    Danny - Add Tile and castle prefabs

--- Tower Defence/Assets/Enemies/EnemyMover.cs
+++ Tower Defence/Assets/Enemies/EnemyMover.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
 
     void Start()
     {
-        PrintWaypointNames();
+        StartCoroutine(FollowPath(waitTime));
     }
 
-    void PrintWaypointNames()
+    IEnumerator FollowPath(float delay)
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(delay);
         }
     }
 }
